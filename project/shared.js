9ccc9682f7724739d8cd75d66c47e43421110fc7
Nathália Pissuti <nathaliapissuti@gmail.com>
2020-10-17 21:02:00 -0300

    Side menu

--- project/shared.js
+++ project/shared.js
@@ -2,6 +2,8 @@ const backdrop = document.querySelector('.backdrop');
 const modal = document.querySelector('.modal');
 const selectPlanButtons = document.querySelectorAll('.plan button');
 const noButton = document.querySelector('.modal__action--negative');
+const toggleButton = document.querySelector('.toggle-button');
+const mobileNav = document.querySelector('.mobile-nav');
 
 selectPlanButtons.forEach(btn => {
     btn.addEventListener('click', () => {
@@ -10,12 +12,19 @@ selectPlanButtons.forEach(btn => {
     });
 });
 
-backdrop.addEventListener('click', () => {
+const closeModal = () => {
     backdrop.style.display = 'none';
     modal.style.display = 'none';
+}
+
+backdrop.addEventListener('click', () => {
+    mobileNav.style.display = 'none';
+    closeModal();
 });
 
-noButton.addEventListener('click', () => {
-    backdrop.style.display = 'none';
-    modal.style.display = 'none';
+noButton.addEventListener('click', closeModal);
+
+toggleButton.addEventListener('click', () => {
+    backdrop.style.display = 'block';
+    mobileNav.style.display = 'block';
 });
